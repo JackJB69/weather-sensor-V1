a4fa3dab071d7cfd8b1240eb9bd5ae04511fa44d
JackJB69 <Jackson.stein7515@gmail.com>
2026-04-23 18:15:18 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,254 @@
+# Python code
+#
+
+def on_forever():
+  if input.temperature() < 1:
+    basic.show_leds("""
+      # . # . #
+      . # # # .
+      # # . # #
+      . # # # .
+      # . # . #
+      """)
+  if input.temperature() > 29:
+    basic.show_leds("""
+      . # . # .
+      # # # # #
+      . # # # .
+      # # # # #
+      . # . # .
+      """)
+  if input.temperature() == 1:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 2:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 3:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 4:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 5:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 6:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 7:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 8:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 9:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 10:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 11:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 12:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 13:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 14:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 15:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 16:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 17:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 18:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 19:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 20:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 21:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 22:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 23:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 24:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 25:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 26:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 27:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 28:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+  if input.temperature() == 29:
+    basic.show_leds("""
+      . # . # .
+      . # # # .
+      # . # . #
+      # . . . #
+      # . . . #
+      """)
+basic.forever(on_forever)
+
---
+++ main.ts
@@ -0,0 +1,281 @@
+input.onButtonPressed(Button.AB, function () {
+    if (input.temperature() < 1) {
+        basic.showLeds(`
+            # . # . #
+            . # # # .
+            # # . # #
+            . # # # .
+            # . # . #
+            `)
+    }
+    if (input.temperature() > 29) {
+        basic.showLeds(`
+            . # . # .
+            # # # # #
+            . # # # .
+            # # # # #
+            . # . # .
+            `)
+    }
+    if (input.temperature() == 1) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 2) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 3) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 4) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 5) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 6) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 7) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 8) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 9) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 10) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 11) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 12) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 13) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 14) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 15) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 16) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 17) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 18) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 19) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 20) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 21) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 22) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 23) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 24) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 25) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 26) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 27) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 28) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() == 29) {
+        basic.showLeds(`
+            . # . # .
+            . # # # .
+            # . # . #
+            # . . . #
+            # . . . #
+            `)
+    }
+})
